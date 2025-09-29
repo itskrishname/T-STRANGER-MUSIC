04acf517d42249e262e040150a98a127af76d034
Krishna <kd259457@gmail.com>
2025-09-29 11:36:09 +0530

    Update Gpt.py

--- SHUKLAMUSIC/plugins/tools/Gpt.py
+++ SHUKLAMUSIC/plugins/tools/Gpt.py
@@ -22,7 +22,7 @@ async def chat_gpt(bot, message):
         if len(message.command) < 2:
             # If no question is asked, send an example message
             await message.reply_text(
-                "❍ ᴇxᴀᴍᴘʟᴇ:**\n\n/chatgpt ᴡʜᴏ ɪs ᴛʜᴇ ᴏᴡɴᴇʀ ᴏғ ˹ sᴛʀᴀɴɢᴇʀ ™˼?"
+                "❍ ᴇxᴀᴍᴘʟᴇ:**\n\n/chatgpt ᴡʜᴏ ɪs ᴛʜᴇ ᴏᴡɴᴇʀ ᴏғ Spotify?"
             )
         else:
             # Extract the query from the user's message
@@ -68,7 +68,7 @@ async def chat_gpt(bot, message):
                     if "choices" in response_data and len(response_data["choices"]) > 0:
                         result = response_data["choices"][0]["message"]["content"]
                         await message.reply_text(
-                            f"{result} \n\nＡɴsᴡᴇʀᴇᴅ ʙʏ➛ sᴛʀᴀɴɢᴇʀ-ᴍᴜsɪᴄ ™",
+                            f"{result} \n\nＡɴsᴡᴇʀᴇᴅ ʙʏ➛ @Lord_Vasudev_Krishna",
                             parse_mode=ParseMode.MARKDOWN
                         )
                     else:
