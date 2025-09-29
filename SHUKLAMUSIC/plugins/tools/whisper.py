6b9fee155e0b5f875f57091f7992491bcdd99e73
Krishna <kd259457@gmail.com>
2025-09-29 20:12:06 +0530

    Update whisper.py

--- SHUKLAMUSIC/plugins/tools/whisper.py
+++ SHUKLAMUSIC/plugins/tools/whisper.py
@@ -110,8 +110,8 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="💒 Whisper",
-            description=f"@SapnaMusicRobot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@SapnaMusicRobot (Target Username or ID) (Your Message).\n\n**Example:**\n@SapnaMusicRobot @username I Wanna Phuck You"),
+            description=f"@Noela_Music [USERNAME | ID] [TEXT]",
+            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@Noela_Music (Target Username or ID) (Your Message).\n\n**Example:**\n@Noela_Music @username I Wanna Phuck You"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
         )
